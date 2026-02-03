808597e840ffdccc69f124f7e9d3c8af283cac1d
ariapandu <ariapandu24@gmail.com>
2026-02-04 00:40:27 +0700

    first commit

---
+++ script.js
@@ -0,0 +1,11 @@
+// Toggle Menu untuk Mobile
+const menuToggle = document.querySelector('.menu-toggle');
+const navLinks = document.querySelector('.nav-links');
+
+if (menuToggle) {
+    menuToggle.addEventListener('click', () => {
+        navLinks.classList.toggle('active');
+    });
+}
+
+console.log("Website Portofolio Siap!");
